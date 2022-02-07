100b2fafca5bdbb28b1834aa33e7084205aae479
Wade Costa <wadeccosta@gmail.com>
2022-02-06 23:24:47 -0800

    Why is it 2022 already

--- Pong Pt.1/Assets/Ball.cs
+++ Pong Pt.1/Assets/Ball.cs
@@ -8,20 +8,12 @@ public class Ball : MonoBehaviour
     public float speed = 5f;
     private int player1score = 0;
     private int player2score = 0;
-    private bool gameOver=false;
+    public float zoom = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
 	    Direction();
-
-	    /*
-	    float sx = Random.Range(0,2) == 0 ? -1 : 1;
-	    float sy = Random.Range(0,2) == 0 ? -1 : 1;
-
-	    GetComponent<Rigidbody>().velocity = new Vector3(speed * sx, speed * sy, 0f);
-	    */
-        
     }
 
     // Update is called once per frame
@@ -30,14 +22,17 @@ public class Ball : MonoBehaviour
 	if(transform.position.x > 12){
 		transform.position = new Vector3(0,0,0);
 		player1score++;
+		zoom = zoom + 0.025f;
 		Debug.Log("P1 Scored \nP1: " + player1score + " P2: " + player2score);
+		Direction();
 	}
 	else if(transform.position.x < -12){
 		transform.position = new Vector3(0,0,0);
 		player2score++;
+		zoom = zoom + 0.025f;
 		Debug.Log("P2 Scored \nP1: " + player1score + " P2: " + player2score);
+		Direction();
 	}
-	    	/*Debug.Log("X : location " + transform.position.x);*/
 
 	if(player1score == 11){
 		Debug.Log("Player 1 won \nThe Score was P1: " + player1score + " P2: " + player2score);
@@ -47,6 +42,7 @@ public class Ball : MonoBehaviour
 		Debug.Log("Player 2 won \nThe Score was P1: " + player1score + " P2: " + player2score);
 		Reset();
 	}
+
         
     }
 
@@ -54,12 +50,14 @@ public class Ball : MonoBehaviour
     {
 	    float sx = Random.Range(0,2) == 0 ? -1 : 1;
             float sy = Random.Range(0,2) == 0 ? -1 : 1;
+	    
 
-            GetComponent<Rigidbody>().velocity = new Vector3(speed * sx, speed * sy, 0f);
+            GetComponent<Rigidbody>().velocity = new Vector3(zoom * speed * sx, zoom * speed * sy, 0f);
     }
 
     void Reset(){
 	    player1score = 0;
 	    player2score = 0;
+	    zoom = 2f;
     }
 }
